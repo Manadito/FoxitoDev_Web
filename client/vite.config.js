44cb6b84de8ca7066b75597084a6323dc1bb9aae
Ivan Torres <ivan.torres.salvador@gmail.com>
2024-03-01 16:14:14 -0500

    additional deployment files

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/FoxitoDev_Web/",
   plugins: [react(), eslint()],
   assetsInclude: ["**/*.glb"], // Don't forget to add this. This ensures Vite treats '.glb' files as static assets and avoid parsing it as if it were a js module
 });
