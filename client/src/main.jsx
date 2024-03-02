41d2050b22aa4a19fe2d243e6b73f4d864ffa568
Ivan Torres <ivan.torres.salvador@gmail.com>
2024-03-02 17:06:44 -0500

    Hasrouter added back

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// Removed BrowserRouter import since it's no longer used here for Github deploy, else add it again.
+import { HashRouter } from "react-router-dom";
 import { AppProvider } from "./context/AppContext.context.jsx";
 import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* Removed BrowserRouter wrapper for Github, else add it back */}
-    <AppProvider>
-      <App />
-    </AppProvider>
+    <HashRouter>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </HashRouter>
   </React.StrictMode>,
 );
