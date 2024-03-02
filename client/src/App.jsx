b115042b90476dbf8db6a8d6394e21f1f98fd816
Ivan Torres <ivan.torres.salvador@gmail.com>
2024-03-02 17:10:08 -0500

    hashrouter changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,18 @@
 import "./App.css";
-import { Route, Routes, HashRouter as Router } from "react-router-dom"; // Wrap the jsx in HashRouter when deploying to github.
+import { Route, Routes, HashRouter } from "react-router-dom";
 import GameCanvas from "./GameCanvas/GameCanvas";
 import ThreeCanvas from "./views/ThreeCanvas/ThreeCanvas";
 
 function App() {
   return (
-    <Router basename={import.meta.env.VITE_APP_BASE_URL}>
+    <HashRouter>
       <div className="grid h-screen w-screen bg-white">
         <Routes>
           <Route path="/" element={<GameCanvas />} />
           <Route path="/headquarters" element={<ThreeCanvas />} />
         </Routes>
       </div>
-    </Router>
+    </HashRouter>
   );
 }
 export default App;
